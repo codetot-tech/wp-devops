e3c82ce4dc1bcb72ec49158f8d01f50c4a777d90
codetot-tech <147888147+codetot-tech@users.noreply.github.com>
2023-12-07 12:26:55 +0700

    Create maintenance.sh

---
+++ scripts/maintenance.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+git status
+
+if [[ $(git status --porcelain) != "" ]]; then
+
+  echo "Backup current DB"
+  wp db export
+
+  echo "Update live plugins"
+  wp plugin update --all
+
+  echo "Commit live plugin changes"
+  git add wp-content/plugins/
+  git commit -m "Update live plugins"
+
+  echo "Update live themes"
+  wp theme update --all
+  git commit -m "Update core themes"
+
+  echo "Update core WP"
+  wp core update
+  git add wp-admin/ wp-includes/ wp-*.php xmlrpc.php index.php
+  git commit -m "Update core WP"
+
+  echo "Finish maintenance task"
+  git status
+
+  echo "Next: Push to production (manual)"
+
+else
+
+  echo "A repository contains uncommit changes."
+
+fi
