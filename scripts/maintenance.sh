154bd1656c3028eae5d8f9978612299e027812b2
codetot-tech <147888147+codetot-tech@users.noreply.github.com>
2023-12-09 14:15:12 +0700

    Update maintenance.sh

--- scripts/maintenance.sh
+++ scripts/maintenance.sh
@@ -1,35 +1,86 @@
 #!/bin/bash
 
-git status
+echo "✨ Are you ready to update your "
+echo "✨ A maintenance script was supported by CODE TOT. Post your issue on https://github.com/codetot-tech/wp-devops/issues if you have any bugs while running it."
 
-if [ -z "$(git status --untracked-files=no --porcelain)" ]; then 
+# Check if WP-CLI is installed
+if [[ -x "$(command -v wp)" ]]; then
+  echo "✅ WP-CLI is installed. You are good to go."
 
-  echo "Backup current DB"
-  wp db export
+  if git rev-parse --is-inside-work-tree &> /dev/null; then
+    echo "✅ Current folder is a Git repository."
+    echo "🚧 Check Git status."
 
-  echo "Update live plugins"
-  wp plugin update --all
+    git status
+    
+    if [ -z "$(git status --untracked-files=no --porcelain)" ]; then 
+    
+      echo "⚡️ Backup current database."
+      wp db export
+    
+      echo "⚡️ Update live plugins"
+      wp plugin update --all
+    
+      if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
+        git add wp-content/plugins/
+        git commit -m "🔨 Update live plugins"
+      else 
+        echo "🦺 There is no theme update."
+      fi
+      
+      echo "⚡️ Update live themes"
+      wp theme update --all
+      
+      if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
+        git add wp-content/themes/
+        git commit -m "🔨 Update core themes"
+      else 
+        echo "🦺 There is no theme update."
+      fi
+    
+      echo "⚡️ Update core WP"
+      wp core update
+    
+      if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
+        git add wp-admin/ wp-includes/ wp-*.php xmlrpc.php index.php
+        git commit -m "🔨 Update core WP"
+      else 
+        echo "🦺 There is no core update."
+      fi
+    
+      echo "⚡️ Update translations"
+      wp language core update
+      wp language theme update --all
+      wp language plugin updatae --all
+    
+      if [ -z "$(git status --untracked-files=no --porcelain)" ]; then
+        git commit -m "🔨 Update translations"
+      else 
+        echo "🦺 There is no translation update."
+      fi
+    
+      echo "🚩 Finish maintenance task."
 
-  echo "Commit live plugin changes"
-  git add wp-content/plugins/
-  git commit -m "Update live plugins"
+      echo "🚧 Check Git status."
 
-  echo "Update live themes"
-  wp theme update --all
-  git commit -m "Update core themes"
+      git status
+    
+      echo "⚡️ Next: Your turn. Push to production (manual)"
+    
+    else
+    
+      echo "🔥 A repository contains uncommit changes. You should manual take care of it before running a script again."
+    
+    fi
 
-  echo "Update core WP"
-  wp core update
-  git add wp-admin/ wp-includes/ wp-*.php xmlrpc.php index.php
-  git commit -m "Update core WP"
+  else
 
-  echo "Finish maintenance task"
-  git status
+    echo "🐛 Current folder is not a Git repository. Exist."
 
-  echo "Next: Push to production (manual)"
+  fi
 
 else
 
-  echo "A repository contains uncommit changes."
+  echo "🐛 WP-CLI is not installed. Ask your hosting provider to install it. Exist"
 
 fi
